62c281fcc58df8fac31506b605e132e23264c20e
贾永强 <jiayongqiang@qiyi.com>
2018-11-14 16:48:45 +0800

    create

---
+++ Cocoapod-Test/Cocoapod_Test.h
@@ -0,0 +1,14 @@
+//
+//  Cocoapod_Test.h
+//  Cocoapod-Test
+//
+//  Created by 贾永强 on 2018/11/14.
+//  Copyright © 2018 JYQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cocoapod_Test : NSObject
+
++ (void)testLog;
+@end
---
+++ Cocoapod-Test/Cocoapod_Test.m
@@ -0,0 +1,17 @@
+//
+//  Cocoapod_Test.m
+//  Cocoapod-Test
+//
+//  Created by 贾永强 on 2018/11/14.
+//  Copyright © 2018 JYQ. All rights reserved.
+//
+
+#import "Cocoapod_Test.h"
+
+@implementation Cocoapod_Test
+
++ (void)testLog {
+    NSLog(@"测试log。。。。。。。。。。。。");
+}
+
+@end
